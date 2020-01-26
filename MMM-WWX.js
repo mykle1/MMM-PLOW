9e5e0caf657feaddf9bc32e70d2540712f2996e9
Mykle <25211804+mykle1@users.noreply.github.com>
2020-01-25 20:40:32 -0500

    Add files via upload

--- MMM-WWX.js
+++ MMM-WWX.js
@@ -8,20 +8,39 @@
 Module.register("MMM-WWX",{
 
 	defaults: {
-			latLong: "", // Your latitude and longitude seperated by a comma
-			tempUnits: "", // us = F or uk for C
-			color: "",
-			font: ""
+		  languages: "",              //
+		  style: "",                  // See Style list
+			latLong: "",                // Your latitude and longitude seperated by a comma
+			title: "",                  // Think of this as a header
+			tempUnits: "",              // us = F or uk for C
+			textColor: "",              // Hex color codes. No #
+			font: "",                   // See font list
+			htColor: "",                // high temp color.  Hex color codes. No #
+			ltColor: "",                // low temp color.  Hex color codes. No #
+			displaySum: "",             // Display Summary, yes or no
+			displayHeader: "",          // yes or no
+			timeColor: "",              // for graph-bar. Hex color codes. No #
+			tempColor: "",              // for graph-bar. Hex color codes. No #
+			currentDetails: "",         // for graph-bar. true or false
+			graphType: "",              // when style is graph
+			lineColor: "",        // when style is graph.  Hex color codes. No #
+			markerColor: "",      // when style is graph.  Hex color codes. No #
 	},
-  iframe_src: "https://darksky.net/widget/default/40.683,-73.9708/us12/en.js?width=100%&height=350&\
-title=New York,NY&textColor=ffffff&bgColor=000000&transparency=false&skyColor=undefined&fontFamily=Default&customFont=&units=us&htColor=333333&ltColor=C7C7C7&\
-displaySum=yes&displayHeader=yes",
-  jswrapper_front:"<html><body><script type='text/javascript' src='",
+
+	// use double quotes outside, single quotes inside
+	// and use \ as the line continuation character
+
+	jswrapper_front:"<html><body><script type='text/javascript' src='",
+
+  /////////////////// Reminder to self, DO NOT edit the next line. Edit this.iframe_src in the getDom///////////////////
+  iframe_src: "https://darksky.net/widget/default/40.683,-74.9708/us12/en.js?width=100%&height=350&title=This is like a header&textColor=ffffff&bgColor=000000&fontFamily=Default&htColor=ffffff&ltColor=00dfff&displaySum=yes&displayHeader=yes", /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	jswrapper_back: "'></script></body><html>",
+
+
 	getStyles: function() {
         return ["MMM-WWX.css"];
     },
-
 
 	getDom: function() {
 
@@ -29,22 +48,35 @@ displaySum=yes&displayHeader=yes",
 		iframe.classList.add("iframe");
 		iframe.style = "border:none";
 		iframe.scrolling = "no";
-		type="text/javascript";
 
-	//	Tomorrow try double quotes interior and single quotes for the entire url
+		if (this.config.style === "default") {
+			iframe.style.width = "620px";
+			iframe.style.height = "310px";
+		}
+		if (this.config.style === "default-small") {
+			iframe.style.width = "144px";
+			iframe.style.height = "210px";
+		}
+		if (this.config.style === "small") {
+			iframe.style.width = "304px";
+			iframe.style.height = "200px";
+		}
+		if (this.config.style === "graph-bar") {
+			iframe.style.width = "800px";
+			iframe.style.height = "400px";
+		}
+		if (this.config.style === "graph") {
+			iframe.style.width = "820px";
+			iframe.style.height = "400px";
+    }
 
-				// use double quotes outside, single quotes inside
-				// and use \ as the line continuation character
-       iframe.srcdoc = this.jswrapper_front+this.iframe_src+this.jswrapper_back
 
+		// type="text/javascript";
+    // insert config options
+    this.iframe_src= "https://darksky.net/widget/"+this.config.style+"/"+this.config.latLong+"/"+this.config.tempUnits+"12/"+this.config.languages+".js?width=100%&height=350&title="+this.config.title+"&textColor="+this.config.textColor+"&bgColor=000000&fontFamily="+this.config.font+"&htColor="+this.config.htColor+"&ltColor="+this.config.ltColor+"&displaySum="+this.config.displaySum+"&displayHeader="+this.config.displayHeader+"&timeColor="+this.config.timeColor+"&tempColor="+this.config.tempColor+"&currentDetailsOption="+this.config.currentDetails+"&graph="+this.config.graphType+"&lineColor="+this.config.lineColor+"&markerColor="+this.config.markerColor+"";
 
-        // smallest
-				// iframe.src = 'https://darksky.net/widget/default-small/'+this.config.latLong+'/'+this.config.tempUnits+'12/en.js?width=120&height=60&title=Full Forecast&textColor='+this.config.color+'&bgColor=000000&skyColor=DCFF4D&fontFamily='+this.config.font+'';
-
-				// smallest with today's forecast
-				// iframe.src = 'https://darksky.net/widget/small/'+this.config.latLong+'/'+this.config.tempUnits+'12/en.js?width=160&height=60&title=Full Forecast&textColor='+this.config.color+'&bgColor=000000&skyColor=DCFF4D&fontFamily='+this.config.font;
-
-
+    // join the parts in the Dom
+    iframe.srcdoc = this.jswrapper_front+this.iframe_src+this.jswrapper_back;
 
 		return iframe;
 	},
--- README.md
+++ README.md
@@ -1,14 +1,39 @@
 ## MMM-WWX
 
-### Under construction
+#### *** Weather Widgets Xtreme! ***
 
-## Should look like this :-)
+* Driven by the Darksky API yet you don't need an API key!
 
-![](images/1.png)
+## Examples
 
-## Currently displays like this :-(
+#### Minimal view - (config style: "default-small",)
 
-![](images/2.png)
+![](images/minimal.png)
+
+#### Minimal plus current conditions - (config style: "small",)
+
+![](images/minimalPlus.png)
+
+#### 7 day forecast - (config style: "default",)
+
+![](images/7day.png)
+
+#### 7 day forecast with current conditions - (config style: "default",)
+
+![](images/7dayPlus.png)
+
+#### Hourly bar graph with or without text forecast - (config style: "graph-bar",)
+
+![](images/barGraph.png)
+
+#### Temp, Precip, Humidity, Wind, Pressure, UV and Visibility line graphs - (config style: "graph" and config graphType: "wind_graph",)
+
+![](images/temperature.png)
+![](images/precip.png)
+
+#### Multiple instances by simply adding more config entries
+
+![](images/multiple.png)
 
 ## Installation
 
@@ -16,16 +41,75 @@
 
 ## Config.js entry and options
 
+
 ```
 {
 disabled: false,
 module: 'MMM-WWX',
 position: 'middle center',
 config: {
-    latLong: "40.683,-74.9708", // Your latitude and longitude seperated by a comma
-    tempUnits: "us", // us = F - uk = C
-    color: "FFFFFF", // Hex color codes
-    font: "Default",  //
-              }
-          },
+   languages: "en",                          // en, de, es, fr, it, nl, ar, zh, x-pig-latin
+   style: "default",                         // See Style list
+   latLong: "12.345,-67.8910",               // Your latitude and longitude seperated by a comma
+   title: "New Dorp",                        // Location seems logical
+   tempUnits: "us",                          // us = F - uk = C
+   textColor: "ffffff",                      // Hex color codes. No # !!!
+   font: "default",                          // See font list
+   htColor: "ffffff",                        // high temp color. Hex color codes. No #
+   ltColor: "00dfff",                        // low temp color. Hex color codes. No #
+   displaySum: "yes",                        // Display Summary, yes or no
+   displayHeader: "yes",                     // yes or no
+   timeColor: "ffffff",                      // for graph-bar. Hex color codes. No #
+   tempColor: "00dfff",                      // for graph-bar. Hex color codes. No #
+   currentDetails: "true",                   // for graph-bar. Hex color codes. No #
+   graphType: "precip_graph",                // when style is graph
+   lineColor: "dd66cf",                      // when style is graph
+   markerColor: "ffffff",                    // when style is graph
+   }
+},
 ```
+
+## Config style list
+
+* small
+* default-small
+* default
+* graph_bar
+* graph
+
+
+## Attention!
+
+* Not all config options apply to every style
+
+## Font List
+
+* Default
+* Trebuchet MS
+* Georgia
+* Times New Roman
+* Arial
+* Verdana
+* Palatino
+* Serif
+* Sans Serif
+
+## Language List
+
+* English en
+* Deutsch de
+* Spanish es
+* French fr
+* Italian it
+* Nederlands nl
+* Arabic ar
+* Chinese zh
+* Pig Latin x-pig-latin
+
+## Attention!
+
+* Not all config options apply to every style
+
+## My unending gratitude to sdetweil and cowboysdude
+
+* Thank you, my friends!
